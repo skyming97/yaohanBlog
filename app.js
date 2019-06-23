951831ce88cbd9a88886d48170a49a3cd0e3fcf8
honyausgt <a13975991339@gmail.com>
2019-06-23 16:18:38 +0800

    登录和注册简单路由，实现模块化

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser")
 
 // 设置 默认采用的模板引擎名称
 app.set('view engine', 'ejs')
@@ -8,22 +9,10 @@ app.set('views', './views')
 
 app.use('/node_modules', express.static('node_modules'))
 
-//======首页
-app.get('/', (req, res) => {
-  res.render('index', {})
-})
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// 注意：当在调用模板引擎的res.render函数的时候，
-// ./相对路径，是相对于app.set（'views'）指定的目录，来进行查找的
-//=====注册
-app.get("/register", (req, res) => {
-  res.render("./user/register.ejs")
-})
-
-//=====登录
-app.get("/login", (req, res) => {
-  res.render("./user/login.ejs")
-})
+const router = require("./router/index.js")
+app.use(router)
 
 app.listen(80, () => {
   console.log("http://127.0.0.1:80")
