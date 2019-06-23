390799a29e01228a574547dec6701d6ae98bbb4f
honyausgt <a13975991339@gmail.com>
2019-06-23 10:58:49 +0800

    登录和注册页页面实现

--- app.js
+++ app.js
@@ -8,12 +8,23 @@ app.set('views', './views')
 
 app.use('/node_modules', express.static('node_modules'))
 
-
+//======首页
 app.get('/', (req, res) => {
   res.render('index', {})
 })
 
+// 注意：当在调用模板引擎的res.render函数的时候，
+// ./相对路径，是相对于app.set（'views'）指定的目录，来进行查找的
+//=====注册
+app.get("/register", (req, res) => {
+  res.render("./user/register.ejs")
+})
 
-app.listen(3000, () => {
-  console.log("服务器运行成功……")
+//=====登录
+app.get("/login", (req, res) => {
+  res.render("./user/login.ejs")
+})
+
+app.listen(80, () => {
+  console.log("http://127.0.0.1:80")
 })
